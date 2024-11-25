073b0dc68586d3b65099aa7c0983b6a2f50d0d70
Nurmi Johnny <s2400330@BC-T2YG2MVD9H-Kannettava-tietokone.local>
2024-11-25 17:08:52 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,12 @@ const App = () => {
   }
 
   const toggleImportanceOf = id => {
+    console.log(`importance of ${id} needs to be toggled`)
     const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
+    const changedNote = { ...note, important: !note.important 
+     
+    }
   
     noteService
       .update(id, changedNote)
