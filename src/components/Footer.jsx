3b866d4aebe6d83c283ca9c6179ba3bd22576aed
Nurmi Johnny <s2400330@BC-T2YG2MVD9H-Kannettava-tietokone.local>
2024-11-27 14:20:28 +0200

    up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,13 @@ const Footer = () => {
     const footerStyle = {
       color: 'green',
       fontStyle: 'italic',
-      fontSize: 16
+      fontSize: 16,
+      marginTop: 20
     }
   
     return (
       <div style={footerStyle}>
-        <br />
+    
         <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
       </div>
     )
